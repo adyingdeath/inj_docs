415e3fdafc5897b7e4167e6be4b566cf2cdad238
CatDevil <2975527435@qq.com>
2024-12-25 12:22:02 +0800

    Complete basics parts of INJ docs.

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -18,7 +18,10 @@ export default async function Page(props: {
   const MDX = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+    toc={page.data.toc}
+    tableOfContent={{style: "clerk", single: false}}
+    full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
