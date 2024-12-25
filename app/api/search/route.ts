4784ee12c8d417ec80caa74a8dd49d3b4d8a798f
CatDevil <2975527435@qq.com>
2024-12-25 13:58:43 +0800

    Return to dynamic mode.

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -4,4 +4,4 @@ import { createFromSource } from 'fumadocs-core/search/server';
 // it should be cached forever
 export const revalidate = false;
 
-export const { staticGET: GET } = createFromSource(source);
+export const { GET } = createFromSource(source);
